22c93020dad81a0f530efade2121d458648e587f
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-05-20 19:12:59 +0100

    task 7 done

--- models/state.py
+++ models/state.py
@@ -1,35 +1,33 @@
 #!/usr/bin/python3
-"""This is the state class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
+
+"""
+A module that defines the ORM class for State table
+"""
+import os
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
-import models
-from models.city import City
-import shlex
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
     """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", cascade='all, delete, delete-orphan',
-                          backref="state")
+    Defines attributes for State table
+    """
+    __tablename__ = 'states'
 
-    @property
-    def cities(self):
-        var = models.storage.all()
-        lista = []
-        result = []
-        for key in var:
-            city = key.replace('.', ' ')
-            city = shlex.split(city)
-            if (city[0] == 'City'):
-                lista.append(var[key])
-        for elem in lista:
-            if (elem.state_id == self.id):
-                result.append(elem)
-        return (result)
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade='all, delete')
+    else:
+        name = ''
+
+        @property
+        def cities(self):
+            """
+            Returns (list): List of City instances
+            with state_id equals to the current State.id
+            """
+            from models.city import City
+            from models import storage
+            city_list = list(storage.all(City).values())
+            return [city for city in city_list if city.state_id == self.id]
