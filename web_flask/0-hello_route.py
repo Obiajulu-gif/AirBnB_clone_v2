2344ca736311427511b75f3de6cb0843a3d65a32
Obiajulu-gif <okoyeemmanuel998@gmail.com>
2024-05-20 13:10:26 +0100

    task 0 done

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,22 @@
+#!/usr/bin/python3
+"""script that starts a Flask web application"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """
+    Defines a route for the root URL ("/") of the Flask application. When a GET request is made to this URL, the function `hello_hbnb` is called. This function returns the string "Hello HBNB!".
+
+    Parameters:
+        None
+
+    Returns:
+        str: The string "Hello HBNB!"
+
+    """
     return "Hello HBNB!"
 
 if __name__ == '__main__':
